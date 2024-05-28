f55d129ef3d74c70a85e654160ecad4760ea6c85
Filipe Nascimento <lipe.nscm@gmail.com>
2024-05-28 16:54:07 -0300

    aula 06-abril

--- db/data_explore.sql
+++ db/data_explore.sql
@@ -1,3 +1,75 @@
+# Agregações Básicas
+-- Contagem utilizando a função COUNT
+select count(*) from tb_prato;
+
+-- Soma de valores
+select p.nome_prato, p.preco_unitario_prato as preco_unitario , count(p.nome_prato) num_pedidos,sum(pd.quantidade_pedido ) quantidade_em_pedidos ,sum(pd.quantidade_pedido * p.preco_unitario_prato) as total
+from tb_pedido pd
+left join tb_prato p 
+on pd.codigo_prato = p.codigo_prato
+left join tb_tipo_prato tp
+on p.codigo_tipo_prato = tp.codigo_tipo_prato
+group by 1,2
+order by 5 desc;
+
+-- Media de valores
+select avg(p.preco_unitario_prato) as preco_medio
+from tb_prato p;
+
+-- Valor Maximo e Minimo
+select max(p.preco_unitario_prato) as maior_preco
+from tb_prato p;
+
+select min(p.preco_unitario_prato) as menor_preco
+from tb_prato p;
+
+# Agregações com Agrupamentos
+select tp.nome_tipo_prato, count(p.nome_prato)
+from tb_prato p
+left join tb_tipo_prato tp
+on p.codigo_tipo_prato = tp.codigo_tipo_prato
+group by 1
+order by 1 ;
+
+#Filtrando Resultados Agregados
+select tp.nome_tipo_prato, count(p.nome_prato)
+from tb_prato p
+left join tb_tipo_prato tp
+on p.codigo_tipo_prato = tp.codigo_tipo_prato
+where tp.codigo_tipo_prato = 2
+group by 1
+order by 1 ;
+
+# Analises Temporais
+-- soma de pedidos por ano
+select year(ms.data_hora_entrada),sum(pd.quantidade_pedido ) quantidade_em_pedidos ,sum(pd.quantidade_pedido * p.preco_unitario_prato) as total
+from tb_pedido pd
+left join tb_prato p 
+on pd.codigo_prato = p.codigo_prato
+left join tb_tipo_prato tp
+on p.codigo_tipo_prato = tp.codigo_tipo_prato
+left join tb_mesa ms
+on pd.codigo_mesa = ms.codigo_mesa
+group by 1
+order by 1 desc;
+
+# Construção de visões
+create view vw_faturamento_ano as 
+select year(ms.data_hora_entrada),sum(pd.quantidade_pedido ) quantidade_em_pedidos ,sum(pd.quantidade_pedido * p.preco_unitario_prato) as total
+from tb_pedido pd
+left join tb_prato p 
+on pd.codigo_prato = p.codigo_prato
+left join tb_tipo_prato tp
+on p.codigo_tipo_prato = tp.codigo_tipo_prato
+left join tb_mesa ms
+on pd.codigo_mesa = ms.codigo_mesa
+group by 1
+order by 1 desc;
+
+
+select * from vw_faturamento_ano;
+
+
 # Quantos clientes o restaurante desde a abertura ?
 select count(*) from tb_cliente;
 
